77e0a1b08995b4c841e623fa53374a9d8beff52c
Tyler Collier <tyler.collier+github@devmounta.in>
2018-02-13 10:42:03 -0700

    Add apiKeyChecker middleware

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/data', (req, res) => {
+function apiKeyChecker(req, res, next) {
+  if (req.headers.apikey === 't$rules') {
+    next();
+  } else {
+    res.status(400).send('You forgot the password, or put the wrong one');
+  }
+}
+
+app.get('/data', apiKeyChecker, (req, res) => {
+  console.log(req.query.name + ' got it!')
   res.json({
     user: 'pretend this is meaningful data, like URLs to pictures of the simpsons',
   });
+  next();
 });
+
+app.get('/secret', apiKeyChecker, (req, res) => {
+  console.log('someone got the secret');
+  res.json({
+    user: 'more pretending'
+  });
+})
 
 const port = 3005;
 app.listen(port, () => {
