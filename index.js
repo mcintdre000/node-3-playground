8d06f1f58a6930a495f185e0f82f0f9257b0bfcc
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-05-22 14:29:04 -0700

    initial commit

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.get('/more-data', (req, res) => {
   });
 });
 
+app.get('/telephone', (req, res) => {
+
+  console.log('got a request')
+  // res.json({ message: 'Vote for Pedro!' })
+  axios.get('http://192.168.1.26:3005/telephone2').then(response => {
+    console.log('response.data', response.data)
+    const {message} = response.data;
+    res.json({ message: 'Vote for Pedro! ' + message })
+  }).catch( error => {console.log('error', error);
+    res.status(500).json({ message: 'something went wrong'})})
+});
+
 const port = 3005;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
